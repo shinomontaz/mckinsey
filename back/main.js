40df0cb43a2825a8b260ddf68498e458acb50db2
shinomontaz@gmail.com <shinomontaz@gmail.com>
2019-11-20 00:26:54 +0300

    v0.1

--- back/main.js
+++ back/main.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
 
-const area = require('./report')
+const report = require('./report')
 
 const app = express()
 app.use(cors());
--- back/report.js
+++ back/report.js
@@ -1,5 +1,8 @@
 const { pool } = require('./pool')
 
+const RESOLVED = 2;
+const BLOCKED = 3;
+
 const getReports = (request, response) => {
 
   var state = parseInt(request.query.state)
@@ -23,7 +26,7 @@ const blockContent = (request, response) => {
   const { id, user } = req.body
 
   // update report to state of Blocked and set is_visible to FALSE for specefic entry
-  pool.query('WITH content_update AS (UPDATE content SET is_visible = FALSE WHERE id = $1 RETURNING id) UPDATE report SET state = 3, fk_user = $2 WHERE fk_content IN (SELECT id FROM content_update)', [id, user], (error, results) => {
+  pool.query('WITH content_update AS (UPDATE content SET is_visible = FALSE WHERE id = $1 RETURNING id) UPDATE report SET state = $2, fk_user = $3 WHERE fk_content IN (SELECT id FROM content_update)', [id, user, BLOCKED], (error, results) => {
     if (error) {
       throw error
     }
@@ -35,9 +38,7 @@ const blockContent = (request, response) => {
 const resolveReport = (request, response) => {
   const { id, user } = req.body
 
-  const id = parseInt(request.params.id)
-
-  pool.query('', [id], (error, results) => {
+  pool.query('UPDATE report SET state = $1, fk_user = $1 WHERE id = $3', [RESOLVED, user, id], (error, results) => {
     if (error) {
       throw error
     }
@@ -47,6 +48,6 @@ const resolveReport = (request, response) => {
 
 module.exports = {
   getReports,
-  updateReport,
+  resolveReport,
   blockContent
 }
